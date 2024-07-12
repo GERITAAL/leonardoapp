8b617b7fb8e30d050cde289eb88d2dd2d36e332c
Taniapino <tvpalaciospi@cesde.net>
2024-07-12 15:55:34 -0500

    se crea logout

--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,3 +1,8 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
+  import { getAuth,onAuthStateChanged,signOut } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js'
+
 function cambiarCaja(){
     let boton=document.getElementById("botonChat")
     let cajaChat=document.getElementById("cajaChat")
@@ -10,3 +15,34 @@ function cambiarCaja(){
 
 }
 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATaFS3y2HtZA4y-3WQm7fRtbsaAycB6uU",
+    authDomain: "loginsuradualjjg.firebaseapp.com",
+    projectId: "loginsuradualjjg",
+    storageBucket: "loginsuradualjjg.appspot.com",
+    messagingSenderId: "270841800332",
+    appId: "1:270841800332:web:99ae746100c68433effda9",
+    measurementId: "G-3NGP4DKQEW"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  let avatar=document.getElementById("avatar")
+  let botonsalir=document.getElementById("botonsalir")
+
+  //utilizando la informacion del usuario loggeado
+  const auth=getAuth()
+  onAuthStateChanged(auth,(user)=>{
+    if(user){
+        let uid=user.uid
+        avatar.classList.remove("invisible")
+        avatar.textContent=user.email
+    }else{
+
+    }
+  })
+
+  botonsalir.addEventListener("click",function(){
+    alert("estas saliendo")
+  })
